bf6c2308d48ba1965d0d5667957d4cb1e855030a
ekultek <thomas.perkins23@icloud.com>
2018-03-23 08:49:53 -0500

    starting compatibility with macos

--- install.sh
+++ install.sh
@@ -9,20 +9,29 @@ echo "|  |  ||     |  |  |  |     |\    ||  |  |     ||     ||  |   |  |  ";
 echo "|__|__| \__,_|  |__|   \___/  \___||__|  |_____| \___/|____|  |__|  ";
 echo "                                                                    ";
 
-function installDebian() {
+function installDebian () {
     sudo apt-get update;
     sudo apt-get -y install git python2.7 python-pip postgresql apache2;
     pip2 install requests psutil;
     installMSF;
 }
 
-function installFedora() {
+function installFedora () {
     sudo yum -y install git python-pip;
     pip2 install requests psutil;
     installMSF;
 }
 
-function installMSF() {
+function installOSX () {
+  sudo /usr/sbin/apachectl start;
+  brew doctor;
+  brew update;
+  brew install postgresql;
+  brew services start postgresql;
+  installMSF;
+}
+
+function installMSF () {
     if [[ ! "$(which msfconsole)" = */* ]]; then
         curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
             chmod 755 msfinstall && \
@@ -31,7 +40,7 @@ function installMSF() {
     fi
 }
 
-function install() {
+function install () {
     case "$(uname -a)" in
         *Debian*|*Ubuntu*)
             installDebian;
@@ -39,8 +48,11 @@ function install() {
         *Fedora*)
             installFedora;
             ;;
+        *Darwin*)
+            installOSX;
+            ;;
         *)
-            echo "Unable to detect Linux flavor...";
+            echo "Unable to detect operating system that is compatible with AutoSploit...";
             ;;
     esac
     echo "";
